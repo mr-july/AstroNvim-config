08ef1250e5fef5ff000d25ec9a85de3c45fb3b49
Igor Lubimov <mr_july@mail.ru>
2023-01-19 10:24:51 +0100

    feat: set the option to wrap long lines by default

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -73,7 +73,7 @@ local config = {
       number = true, -- sets vim.opt.number
       spell = false, -- sets vim.opt.spell
       signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-      wrap = false, -- sets vim.opt.wrap
+      wrap = true, -- sets vim.opt.wrap
     },
     g = {
       mapleader = " ", -- sets vim.g.mapleader
