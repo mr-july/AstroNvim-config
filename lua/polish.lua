b2007ae3085dc9aa9ede3a73a2aeca628e812a58
Mr. JULY <mr_july@mail.ru>
2024-07-28 19:33:56 +0200

    feat: define custom functions
    - custom keymap toggling;
    - command line abbreviation for search/replace with increment.

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,18 +1,17 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
--- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+--- Create custom command to toggle the keymap
+vim.api.nvim_create_user_command("ToggleKeymap", function()
+  if vim.bo.keymap == "" then
+    vim.bo.keymap = "deru"
+  else
+    vim.bo.keymap = ""
+  end
+  vim.notify(string.format("keymap='%s'", vim.bo.keymap))
+end, { desc = "Toggle the keymap (none/'deru')" })
+
+-- Abbreviations:
+-- replace the last search string occurrences by numbers 1, 2, 3...
+vim.cmd "cabbrev inc let @a=1 \\| %s##\\=''.(@a+setreg('a', @a + 1)).''#g"
